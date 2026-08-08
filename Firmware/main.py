76b14169f027742ef7773ce0f4eb47e97708cf62
GavinWu42711 <gavinwu42711@gmail.com>
2026-08-08 16:32:30 -0400

    Update
    Updating firmware

--- Firmware/main.py
+++ Firmware/main.py
@@ -5,12 +5,12 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
+from kmk.modules.macros import Macros
 from kmk.modules.macros import Press, Release, Tap, Macros
-from kmk.extensions.LED import statusLED
+from kmk.extensions import statusLED
 
 #For the display
 from kmk.modules.layers import Layers
-keyboard.modules.append(Layers())
 
 #For OLED display
 from kmk.extensions.display import Display, TextEntry, ImageEntry
@@ -19,9 +19,10 @@ import busio
 
 #This is the main instance of your keyboard
 keyboard = KMKKeyboard()
+keyboard.modules.append(Layers())
 
 #Adding display extensions
-i2c_bus = busio.I2C(board.GP_SCL, board.GP_SDA)
+i2c_bus = busio.I2C(board.D5, board.D4)
 driver = SSD1306(i2c = i2c_bus)
 display = Display(display = driver, width = 128, height = 32, brightness = 0.8)
 display.entries = [
@@ -39,11 +40,11 @@ macros = Macros()
 keyboard.modules.append(macros)
 
 #Add the statusLED extension
-statusLed = statusLED(led_pin = [board.GP_29], brightness = 30)
-keyboard.extensions.append(statusLED)
+#statusLed = statusLED(led_pin = [board.D3], brightness = 30)
+#keyboard.extensions.append(statusLED)
 
 #Define your pins here!
-PINS = [board.GP_27, board.GP_2, board.GP_3, board.GP_26,board.GP_1,board.GP_4]
+PINS = [board.D0, board.D10, board.D8, board.D7,board.D9,board.D1]
 
 #Tell kmk we are not using a key matrix
 keyboard.matrix = KeysScanner(
@@ -55,51 +56,50 @@ keyboard.matrix = KeysScanner(
 #Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
 #And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
 keyboard.keymap = [[
-    KC.Macro(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))],
+    KC.MACRO(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))],
     
     #To prevent soft-locking from changes in layers
     [
-    KC.Macro(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
+    KC.MACRO(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
 ],[
-    KC.Macro(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
+    KC.MACRO(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
 ],[
-    KC.Macro(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
+    KC.MACRO(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
 ],[
-    KC.Macro(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
+    KC.MACRO(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
 ],[
-    KC.Macro(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
-    KC.Macro(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
-    KC.Macro(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
+    KC.MACRO(KC.TO(0), Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(1), Press(KC.LCMD), Tap(KC.C), Release(KC.LCMD)),  
+    KC.MACRO(KC.TO(2), Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(3), Press(KC.LCMD), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(4), Press(KC.LCMD(KC.LSFT)), Tap(KC.TAB), Release(KC.LCMD)), 
+    KC.MACRO(KC.TO(5), Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD))
 ]]
 
-#Start kmk!
-if __name__ == 'main':
-    keyboard.go()
+keyboard.go()
+
